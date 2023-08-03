f3f7cad3f5f74e341bd4271ae9b76d3ff3ce02e5
Abhijeet gitte <abhijeetgitte85@gmail.com>
2023-08-03 17:21:27 +0530

    my first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import Testimonial from "./Components/Testimonial";
+import datas from "./data";
+
+function App() {
+  return (
+    <div>
+      <div className="flex flex-col w-[100vw] min-h-[100vh] justify-center items-center bg-gray-200 py-12">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">Our Testimonials</h1>
+          <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+
+          <Testimonial datas={datas} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
